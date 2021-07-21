64beff26cd183591d2f361a167350739eaa2ca6a
imsaptarshi <123saptarshi.basu@gmail.com>
2021-07-21 22:56:01 +0530

    init

--- routes/browserJob.ts
+++ routes/browserJob.ts
@@ -11,7 +11,39 @@ router.route("/").get((req: express.Request | any, res: express.Response) => {
         property="og:image:alt"
         content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}"
       />
-      <meta property="og:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
+            <meta property="og:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
+            <meta name="title" content="Capturr" />
+            <meta
+                property="og:url"
+                content=""
+              />
+            <meta
+                name="description"
+                content="${req.query.url}"
+              />
+              <meta property="og:type" content="website" />
+              <meta
+                property="og:title"
+                content="Orbits - Research | Learn | Share"
+              />
+              <meta
+                property="og:description"
+                content="${req.query.url}"
+              />
+              <meta property="twitter:card" content="summary_large_image" />
+              <meta
+                property="twitter:url"
+                content=""
+              />
+              <meta
+                property="twitter:title"
+                content="Capturr"
+              />
+              <meta
+                property="twitter:description"
+                content="${req.query.url}"
+              />
+              <meta property="twitter:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
       </head>
       <body>ok and</body>`
     )
