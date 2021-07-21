a5fff53177c7ece097454080d90530628c7e76c0
imsaptarshi <123saptarshi.basu@gmail.com>
2021-07-21 23:33:54 +0530

    init

--- routes/browserJob.ts
+++ routes/browserJob.ts
@@ -7,45 +7,46 @@ router.route("/").get((req: express.Request | any, res: express.Response) => {
     res.send(
         `
         <head>
-        <meta
-        property="og:image:alt"
-        content="${req.query.url}"
-      />
+            <meta
+                property="og:image:alt"
+                content="${req.query.url}"
+            />
             <meta property="og:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
             <meta name="title" content="Capturr" />
             <meta
                 property="og:url"
                 content=""
-              />
+            />
             <meta
                 name="description"
                 content="${req.query.url}"
-              />
-              <meta property="og:type" content="website" />
-              <meta
+            />
+            <meta property="og:type" content="website" />
+            <meta
                 property="og:title"
                 content="Capturr"
-              />
-              <meta
+            />
+            <meta
                 property="og:description"
                 content="${req.query.url}"
-              />
-              <meta property="twitter:card" content="summary_large_image" />
-              <meta
+            />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta
                 property="twitter:url"
                 content=""
-              />
-              <meta
+            />
+            <meta
                 property="twitter:title"
                 content="Capturr"
-              />
-              <meta
+            />
+            <meta
                 property="twitter:description"
                 content="${req.query.url}"
-              />
-              <meta property="twitter:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
+            />
+            <meta property="twitter:image" content="${req.API_ENDPOINT}/?api_key=${req.API_KEY}&url=${req.query.url}" />
       </head>
-      <body>ok and</body>`
+      <body>ok and</body>
+      `
     )
 })
 
